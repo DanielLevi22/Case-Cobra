122b25bb1afe5c3a3c1016b79eda515aad7b6c5e
Daniel Levi <daniellevi000@gmail.com>
2024-05-07 11:10:22 -0300

    feaet: upload in progress

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -5,6 +5,8 @@ import { ReactNode } from "react";
 
 export default function Layout({children}: {children: ReactNode}) {
   return (
-      <MaxWidthWrapper className="flex-1 flex-col ">{children}</MaxWidthWrapper>
+    <MaxWidthWrapper className='flex-1 flex flex-col'>
+      {children}
+    </MaxWidthWrapper>
   )
 }
--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,10 +1,15 @@
 "use client"
+import { Progress } from "@/components/ui/progress"
 import { cn } from "@/lib/utils"
-import { useState } from "react"
+import { Image, Loader2, MousePointerSquareDashed } from "lucide-react"
+import { useState, useTransition } from "react"
 import Dropzone, { FileRejection} from "react-dropzone"
 
 export default function Page() {
   const [isDragOver, setIsDragOver] = useState<boolean>(false)
+  const [uploadProgress, setUploadProgress] = useState<number>(0)
+  const [isPending, startTransition] = useTransition()
+  const isUploading = false
 
   function onDropRejected() {}
   function onDropAccepted() {}
@@ -12,10 +17,14 @@ export default function Page() {
 
 
   return (
-    <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center", {
-      "ring-blue-90/25 bg-blue-900/10": isDragOver
-    })}>
-      <div className="relative flex flex-1 flex-col items-center justify-center w-full">
+    <div
+    className={cn(
+      'relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
+      {
+        'ring-blue-900/25 bg-blue-900/10': isDragOver,
+      }
+    )}>
+      <div className='relative flex flex-1 flex-col items-center justify-center w-full'>
         <Dropzone 
           onDropRejected={onDropRejected} 
           onDropAccepted={onDropAccepted}
@@ -28,8 +37,30 @@ export default function Page() {
           onDragLeave={() => setIsDragOver(false)}
         >
           {({getRootProps, getInputProps}) => (
-            <div className="h-full w-full flex-1 flex-col items-center justify-center" {...getRootProps()}>
+            <div  className='h-full w-full flex-1 flex flex-col items-center justify-center' {...getRootProps()}>
               <input {...getInputProps()} />
+              {isDragOver ? (
+                <MousePointerSquareDashed className='h-6 w-6 text-zinc-500 mb-2' />
+              ) : isUploading || isPending ? (
+                <Loader2 className='animate-spin h-6 w-6 text-zinc-500 mb-2' />
+              ) : (
+                <Image className='h-6 w-6 text-zinc-500 mb-2' />
+              )}
+              <div className="flex flex-col items-center mb-2 text-sm text-zinc-700">
+                {isUploading 
+                ? (
+                  <div className="flex flex-col items-center">
+                    <p>Uploading....</p>
+                      <Progress value={uploadProgress} className="mt-2 w-40 bg-gray-300" />
+                  </div>
+                ) 
+                : isPending 
+                ? (<div></div>) 
+                : isDragOver 
+                ? (<span></span>) 
+                : (<span></span>)
+                }
+              </div>
             </div>
           )}
           
