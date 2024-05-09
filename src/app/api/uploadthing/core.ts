3da228c5842192f72b5ab3a36fbadb62c34038ed
Daniel Levi <daniellevi000@gmail.com>
2024-05-08 23:12:31 -0300

    feat: resize image

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,8 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
  import { z } from 'zod'
 const f = createUploadthing();
- 
+ import sharp from 'sharp'
+import { db } from "@/db/prisma";
 const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
  export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
@@ -12,8 +13,35 @@ const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
      return { input }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-
       const { configId } = metadata.input
+
+      const res = await fetch(file.url)
+      const buffer = await res.arrayBuffer()
+
+      const imgMetadata = await sharp(buffer).metadata()
+      const  { width , height} = imgMetadata
+
+      if(!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500
+          }
+        })
+        return { configId: configuration.id }
+      }else {
+        const updateConfiguration = await db.configuration.update({
+          where: {
+            id: configId
+          },
+          data: {
+            croppedImageUrl: file.url
+          }
+        })
+        return { configId: updateConfiguration.id}
+      }
+
       return { configId };
     }),
 } satisfies FileRouter;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,6 +1,34 @@
+import { db } from "@/db/prisma"
+import { notFound } from "next/navigation"
+import { DesignConfigurator } from "./DesignConfigurator"
 
-export default async function Page() {
+interface PageProps {
+  searchParams:{
+    [key:string]: string | string[] | undefined
+  }
+}
+export default async function Page({ searchParams }:PageProps) {
+  const  { id } = searchParams
+
+  if(!id || typeof id !== 'string') {
+    return notFound()
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: {
+      id
+    }
+  })
+
+  if(!configuration) {
+    return notFound()
+  }
+
+
+  const { imageUrl, height, width } = configuration
+
+
   return (
-    <div>page</div>
+    <DesignConfigurator configId={id} imageUrl={imageUrl} imageDimensions={{height, width}} />
   )
 }
