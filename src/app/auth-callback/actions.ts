4dda3f0109abe0dff709a044c0dade291ff60e0c
Daniel Levi <daniellevi000@gmail.com>
2024-05-14 09:06:05 -0300

    fix: provider pagato striper

--- src/app/auth-callback/actions.ts
+++ src/app/auth-callback/actions.ts
@@ -6,29 +6,24 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 
 export async function getAuthStatus() {
   const { getUser } = getKindeServerSession()
-
   const user = await getUser()
 
-  if(!user?.id || !user?.email) {
-    throw new Error("Invalid user data")
+  if (!user?.id || !user.email) {
+    throw new Error('Invalid user data')
   }
 
   const existingUser = await db.user.findFirst({
-    where: {
-      id: user.id
-    }
+    where: { id: user.id },
   })
 
-  if(!existingUser) {
+  if (!existingUser) {
     await db.user.create({
       data: {
         id: user.id,
-        email: user.email
-      }
-    })  
+        email: user.email,
+      },
+    })
   }
 
-  return {
-    success: true
-  }
+  return { success: true }
 }
